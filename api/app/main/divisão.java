a43e1a1af85e525795786650084e3bc046ecf2f2
Seilaqualusername <clso1@aluno.ifal.edu.br>
2022-09-12 17:30:43 +0000

    mainmain

---
+++ api/app/main/divisão.java
@@ -0,0 +1,20 @@
+package app;
+
+import io.jooby.annotations.*;
+import io.jooby.exception.BadRequestException;
+
+@Path("/Divisão/{op}/{op2}")
+public class divisão {
+    
+    @GET
+    public double calcula(@PathParam("op") String opStr, String opStr2) {
+        try {
+            double op = Double.parseDouble(opStr2);
+            double op = Double.parseDouble(opStr);
+            return Math.sqrt(op);
+            return Math.sqrt(op2);
+        } catch (NumberFormatException nfe) {
+            throw new BadRequestException("Operador invalido.");
+        }
+    }
+}
