146bbaa33eeaba2159daa694ff9a0d9131601307
Camila Pinheiro <mcsp3@aluno.ifal.edu.br>
2022-09-26 17:24:11 +0000

    subnew

---
+++ bin/api/src/main/java/app/Subtracao.java
@@ -0,0 +1,24 @@
+package app;
+
+import io.jooby.annotations.*;
+import io.jooby.exception.BadRequestException;
+
+@Path("/sub/{sub1Str}/{sub2Str}")
+public class Subtracao {
+    
+    @GET
+    public double calcula(@PathParam String sub1Str, @PathParam String sub2Str) {
+        try {
+
+        //Criando variáveis
+        double sub1 = Double.parseDouble(sub1Str);
+        double sub2 = Double.parseDouble(sub2Str);
+
+            double result = sub1 - sub2;
+            return result;
+            
+        } catch (NumberFormatException nfe) {
+            throw new BadRequestException("Operação inválida, por favor tente novamente.");
+        }
+    }
+}
