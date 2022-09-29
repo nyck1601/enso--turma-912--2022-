764f424cacef9727a492a32c04d913f299de12c1
Camila Pinheiro <mcsp3@aluno.ifal.edu.br>
2022-09-29 16:25:55 +0000

    mudando

--- bin/api/src/test/java/app/somaTest.java
+++ bin/api/src/test/java/app/somaTest.java
@@ -21,6 +21,13 @@ public class somaTest {
         assertEquals(6.0, resultado);
     }
 
+    @Test
+    public void verificarCasoBase() {
+        Soma soma = new Soma();
+        double resultado = soma.calcula("5","8");
+        assertEquals(13.0, resultado);
+    }
+
     
     }
 
