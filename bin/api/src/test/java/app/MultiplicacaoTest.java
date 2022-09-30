525c10a57f520989221b8a25c2b21e9d031b23e2
JousicleciaAlmeida <jas68@aluno.ifal.edu.br>
2022-09-30 11:58:44 +0000

    mulTest

---
+++ bin/api/src/test/java/app/MultiplicacaoTest.java
@@ -0,0 +1,25 @@
+package app;
+import org.junit.jupiter.api.Test;
+
+import io.jooby.exception.BadRequestException;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class MultiplicacaoTest {
+
+    @Test
+    public void verificaCasoBase() {
+        Multiplicacao mult = new Multiplicacao();
+        double resultado = mult.calcula("2","2");
+        assertEquals(4.0, resultado);
+    }
+
+       
+}
