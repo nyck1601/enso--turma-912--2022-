d80491ac5dd7a05ffa78279be94542b5f2966273
JousicleciaAlmeida <jas68@aluno.ifal.edu.br>
2022-10-03 11:36:24 +0000

    balalaa

--- bin/api/src/test/java/app/MultiplicacaoTest.java
+++ bin/api/src/test/java/app/MultiplicacaoTest.java
@@ -20,6 +20,12 @@ public class MultiplicacaoTest {
         double resultado = mult.calcula("4","2");
         assertEquals(8.0, resultado);
     }
+    @Test
+    public void verificarCasoDouble() {
+        Multiplicacao mult = new Multiplicacao();
+        double resultado = mult.calcula("2.5","2.3");
+        assertEquals(5.75, resultado);
+    }
 
     
     }
