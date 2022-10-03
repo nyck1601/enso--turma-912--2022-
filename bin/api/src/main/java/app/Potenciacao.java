9472d7d0165e414f000ff5e4c0636d50982353cc
Andinhoo-kiddo <acsj3@aluno.ifal.edu.br>
2022-10-03 19:06:31 +0000

    addPotenciação

---
+++ bin/api/src/main/java/app/Potenciacao.java
@@ -0,0 +1,27 @@
+
+package app;
+
+import io.jooby.annotations.*;
+import io.jooby.exception.BadRequestException;
+
+@Path("/potenciação/{div1}/{div2}")
+public class Potenciacao {
+    
+    @GET
+    public double calcula(@PathParam("nf") String div1Str, @PathParam("ns") String div2Str) {
+        try {
+
+        //Criando variáveis
+        double div1 = Double.parseDouble(div1Str);
+        double div2 = Double.parseDouble(div2Str);
+
+            double result = (float)Math.pow(div1, div2);
+            return result;
+            
+        } catch (NumberFormatException nfe) {
+            throw new BadRequestException("Operador inválido.");
+        }
+    }
+}
+
+
